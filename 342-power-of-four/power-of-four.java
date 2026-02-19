987c5e66c4a67791fa535797f21080d858653542
Mithuna Thirunagari <thirunagarimithuna1@gmail.com>
2026-02-19 10:09:00 +0530

    Time: 1 ms (89.34%) | Memory: 42.7 MB (52.40%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n<=0){
-            return false;
-        }
-        if((n&(n-1))!=0){
-            return false;
-        }
-        int mask = 0x55555555;
-        return ((n&mask) != 0);
+        // if(n<=0){
+        //     return false;
+        // }
+        // if((n&(n-1))!=0){
+        //     return false;
+        // }
+        // int mask = 0x55555555;
+        // return ((n&mask) != 0);
+        return (n>0) && ((n&(n-1)) ==0) && ((n & 0x55555555) !=0);
     }
 }
